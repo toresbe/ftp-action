e312a0c82cf59430dca8975ae681050f2abb08b1
Tore Sinding Bekkedal <toresbe@protonmail.com>
2020-07-05 19:48:40 +0200

    wonder if this works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-allow $INPUT_ALLOWSSL; set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --verbose --ignore-time --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-allow $INPUT_ALLOWSSL; set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --verbose --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR --exclude-glob=static/art/**; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-allow $INPUT_ALLOWSSL; set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --verbose --ignore-time --dereference -x ^\.git/$ $INPUT_LOCALDIR/static/art $INPUT_REMOTEDIR/static/art; quit"
