f53e07c60f802fab276492dab369bfb896251326
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2020-07-05 17:20:09 +0200

    nevermind

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-lftp -v $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-allow $INPUT_ALLOWSSL; set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-allow $INPUT_ALLOWSSL; set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; mirror --reverse --continue --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
